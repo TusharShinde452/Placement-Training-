c8dadb9cf1a480389c34ebe2f0aed4e375cf1e68
ASUS <tusharshinde4521@gmail.com>
2026-06-04 16:22:00 +0530

    sum of all even num till user given number

---
+++ src/SumOfEvenNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfEvenNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter num to sum even number till your num:");
+        int num=sc.nextInt();
+        int sum=0;
+        for (int i=1;i<=num;i++){
+            if (i%2==0){
+               sum=sum+i;
+            }
+        }
+        System.out.println("sum of all even num till "+num+" is "+sum);
+    }
+}
